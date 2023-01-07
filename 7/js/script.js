9e74460d570f7b2d20e8f98e5a3b6a4d7ae01113
Keks <keks@htmlacademy.ru>
2023-01-07 12:15:38 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/script.js
@@ -0,0 +1,37 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+const modalOpenBtn = document.querySelector('.form__button');
+const modal = document.querySelector('.modal');
+const modalCloseBtns = document.querySelectorAll('.modal__close-button');
+
+modalOpenBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+modal.classList.add('modal__is-open');
+});
+
+for (let modalCloseBtn of modalCloseBtns) {
+  modalCloseBtn.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  modal.classList.remove('modal__is-open');
+  });
+}
+
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    modal.classList.remove('modal__is-open');
+  }
+});
